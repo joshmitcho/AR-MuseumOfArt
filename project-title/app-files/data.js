3f6d0cd34606731e36dde1a1048962367727a79d
Josh Mitchell <josh@joshpmitchell.ca>
2020-08-24 16:02:40 -0400

    Add Marzipano Demo

---
+++ project-title/app-files/data.js
@@ -0,0 +1,100 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-dscn0003",
+      "name": "DSCN0003",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1936,
+      "initialViewParameters": {
+        "yaw": 3.0421928860296976,
+        "pitch": 0.08674004796668022,
+        "fov": 1.4651895139949895
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.11963009619265,
+          "pitch": 0.17668454330288696,
+          "rotation": 7.0685834705770345,
+          "target": "1-dscn0007"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-dscn0007",
+      "name": "DSCN0007",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1936,
+      "initialViewParameters": {
+        "yaw": 2.1997612987780792,
+        "pitch": 0.21631908825584745,
+        "fov": 1.4651895139949895
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.9123345065244512,
+          "pitch": 0.25122529371315494,
+          "rotation": 0,
+          "target": "0-dscn0003"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.2524249464224368,
+          "pitch": 0.027498106682694257,
+          "title": "Piece #1",
+          "text": "Isn't this piece Dandy?"
+        },
+        {
+          "yaw": -0.2874606712859489,
+          "pitch": 0.05436703632596007,
+          "title": "Piece #2",
+          "text": "Also a good one"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
