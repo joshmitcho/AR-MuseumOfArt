d0d643709f6f40404fe790a8c3ba2332754dbe6f
Josh Mitchell <josh@joshpmitchell.ca>
2020-09-02 21:54:52 -0400

    add second reveal button

--- project-title/app-files/hotspot-styles/js/reveal.js
+++ project-title/app-files/hotspot-styles/js/reveal.js
@@ -16,3 +16,5 @@
 'use strict';
 
 scene.hotspotContainer().createHotspot(document.querySelector("#reveal"), { yaw: -0.92, pitch: -0.1 });
+
+scene.hotspotContainer().createHotspot(document.querySelector("#reveal"), { yaw: 0.92, pitch: 0.1 });
